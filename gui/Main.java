2f98a8103239300e226c226d51c01198dbe0fd60
Julia <juja.mueller@gmail.com>
2018-07-04 15:29:58 +0200

    ..

--- gui/Main.java
+++ gui/Main.java
@@ -1,7 +1,10 @@
 package gui;
 
+import data.Verwaltung;
+
 public class Main {
 	public static void main( String args[]) {
+		load(Verwaltung unique);
 		Hauptfenster neuesfenster = new Hauptfenster();
 	}
 
