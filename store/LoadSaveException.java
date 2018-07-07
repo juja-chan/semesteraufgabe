8c00bdcd44b67de9e4daf0e3cfc27aa1192b0b09
Chieme <chieme.hangen@gmail.com>
2018-07-07 03:57:24 +0200

    Bessere Übersicht und Fehlerbehandlung

--- store/LoadSaveException.java
+++ store/LoadSaveException.java
@@ -4,4 +4,8 @@ public class LoadSaveException extends Exception {
 	public LoadSaveException(String message, Exception cause){
 		super(message, cause);
 	}
+	
+	public LoadSaveException(String message){
+		super(message);
+	}
 }
